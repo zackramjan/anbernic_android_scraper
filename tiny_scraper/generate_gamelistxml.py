3ce188d8c9706351aa00880dcd39de48086c2405
zackramjan <zack.hr@gmail.com>
2024-11-08 20:49:01 -0500

    escape xml values

--- tiny_scraper/generate_gamelistxml.py
+++ tiny_scraper/generate_gamelistxml.py
@@ -3,6 +3,7 @@ import app
 from scraper import Scraper
 from systems import get_system_id
 from pathlib import Path
+from xml.sax.saxutils import escape,quoteattr
 
 scraper = Scraper()
 
@@ -19,10 +20,11 @@ def main():
         for rom in roms_list:
             f.write("<game>\n")
             imgPath = Path(f"{rom.filename}")
-            f.write(f"   <path>{rom.filename}</path>\n")
-            f.write(f"   <name>{rom.filename}</name>\n")
-            f.write(f"   <desc>{rom.filename}</desc>\n")
-            f.write(f"   <image>{scraper.imagedir}/{imgPath.stem}.png</image>\n")
+            romNameXML = escape(rom.filename)
+            f.write(f"   <path>{romNameXML}</path>\n")
+            f.write(f"   <name>{romNameXML}</name>\n")
+            f.write(f"   <desc>{romNameXML}</desc>\n")
+            f.write(f"   <image>{scraper.imagedir}/{escape(imgPath.stem)}.png</image>\n")
             f.write("</game>\n")
         f.write("<gameList>\n")
         f.close()
